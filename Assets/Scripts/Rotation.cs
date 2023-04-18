d0b3719b35ca2c463c7574307fb0d654e5a31649
Anthony Romero <anmirom88@gmail.com>
2023-04-18 07:49:38 -0500

    Added Coins, Material, and a Rotator
    I still need to incorporate Ontrigger for coin, Score text, and particles

--- Assets/Scripts/Rotation.cs
+++ Assets/Scripts/Rotation.cs
@@ -2,17 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestForKraken : MonoBehaviour
+public class Rotation : MonoBehaviour
 {
+
+    public int rotateSpeed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rotateSpeed = 2;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(0, rotateSpeed, 0, Space.World);
     }
 }
